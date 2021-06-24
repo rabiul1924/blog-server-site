04c53a7a0ff2f9b8ff8b9039aaef9cf36a5128ff
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-06-24 19:53:12 +0600

    first step ok

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL,
+ {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true,
+     useCreateIndex:true
+    }).then(console.log('Connected to MongoDb'))
+       .catch((err) => console.log(err));
+
+       
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.use("/api/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
